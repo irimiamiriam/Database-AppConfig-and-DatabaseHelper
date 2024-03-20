09ef4c52bf233030be3e8aef4871728fefbd87fd
Miriam <Miriam@DESKTOP-5ES0VRE>
2024-03-20 18:31:53 +0200

    Add project files.

---
+++ DataAccess/DataBaseHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_SqlDataAdapter_Dataset.DataAccess
+{
+     class DataBaseHelper
+    {
+        private static readonly string _connectionString =  SqlDataAccess.GetConnectionString();
+        private static readonly string _cartiString = SqlDataAccess.GetCartiPath();
+        public static DataSet CartiDataSet;
+
+        public static void Initialisation()
+        {
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                InserareCarti(conn);
+
+            }
+        }
+
+        private static void InserareCarti(SqlConnection conn)
+        {
+            string cmdText = "INSERT INTO Books (Titlu, Autor, Gen) VALUES (@titlu, @autor, @gen)";
+            string deleteText = "DELETE FROM BOOKS";
+            using (SqlCommand cmd= new SqlCommand (deleteText, conn)) { 
+                cmd.ExecuteNonQuery();
+            }
+            using (StreamReader reader = new StreamReader(_cartiString))
+            {
+                while(reader.Peek()>=0) { 
+                    string line = reader.ReadLine();
+                    var splitedLine = line.Split('*');
+                    using(SqlCommand cmd = new SqlCommand(cmdText, conn))
+                    {
+                        cmd.Parameters.AddWithValue("titlu", splitedLine[0]);
+                        cmd.Parameters.AddWithValue("autor", splitedLine[1]);
+                        cmd.Parameters.AddWithValue("gen", splitedLine[2]);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+    }
+
+}
---
+++ DataAccess/SqlDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_SqlDataAdapter_Dataset.DataAccess
+{
+    class SqlDataAccess
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        }
+        public static string GetCartiPath()
+        {
+            return ConfigurationManager.AppSettings["Carti"];
+        }
+    }
+}
